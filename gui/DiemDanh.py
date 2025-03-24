8bd4996994fdb011817e9c9e2b8f785ed83fab74
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2025-03-24 09:30:22 +0700

    ad

--- gui/DiemDanh.py
+++ gui/DiemDanh.py
@@ -4,12 +4,16 @@ from PIL import Image, ImageTk
 import sys
 import os
 import cv2,os
-
+import pandas as pd
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from dao import AttendancesDAO, ClassDAO, DepartmentDAO, StudentDAO
+import datetime
+import time
+
 
 from dao.TestDAO import TestDAO
 from models.Students import Student
+from models.Attendances import Attendances
 class DiemDanh(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -104,7 +108,44 @@ class DiemDanh(tk.Tk):
         self.tree.heading(col, command=lambda: self.treeview_sort_column(col, not reverse))
     
     def diem_danh(self):
-        print("DDiemr danh nhe em")
+        recognizer = cv2.face.LBPHFaceRecognizer_create()#cv2.createLBPHFaceRecognizer()
+        recognizer.read("TrainingImageLabel\Trainner.yml")
+        harcascadePath = "haarcascade_frontalface_default.xml"
+        faceCascade = cv2.CascadeClassifier(harcascadePath);    
+        # df=pd.read_csv("StudentDetails\StudentDetails.csv")
+        cam = cv2.VideoCapture(0)
+        font = cv2.FONT_HERSHEY_SIMPLEX        
+        col_names =  ['Id','Name','Date','Time']
+        attendance = pd.DataFrame(columns = col_names)    
+        while True:
+            ret, im =cam.read()
+            gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+            faces=faceCascade.detectMultiScale(gray, 1.2,5)    
+            for(x,y,w,h) in faces:
+                cv2.rectangle(im,(x,y),(x+w,y+h),(225,0,0),2)
+                Id, conf = recognizer.predict(gray[y:y+h,x:x+w])                                   
+                if(conf < 50):
+                    student=StudentDAO.get_by_id(Id)
+                    ts = time.time()      
+                    date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+                    timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+                   # aa=df.loc[df['Id'] == Id]['Name'].values
+                    tt=str(Id)+"-"+student.fullname
+                    attendance.loc[len(attendance)] = [Id,student.fullname,date,timeStamp]
+                    attendances=Attendances(id=None, class_id=student.class_id, student_id=student.id, status="Điểm danh thành công", checkin_time=timeStamp)
+                    AttendancesDAO.save(attendances=attendances)
+                    
+                else:
+                    Id='Unknown'                
+                    tt=str(Id)             
+                cv2.putText(im,str(tt),(x,y+h), font, 1,(255,255,255),2)        
+            attendance=attendance.drop_duplicates(subset=['Id'],keep='first')    
+            cv2.imshow('im',im) 
+            if (cv2.waitKey(1)==ord('q')):
+                break
+        cam.release()
+        cv2.destroyAllWindows()
+        self.refresh_list()
         
 if __name__ == '__main__':
     app = DiemDanh()
