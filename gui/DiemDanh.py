721d7d0e9393ccc529b6507d4233c91dde981cc4
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2025-04-27 23:07:07 +0700

    add

--- gui/DiemDanh.py
+++ gui/DiemDanh.py
@@ -22,7 +22,7 @@ class DiemDanh(tk.Frame):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.parent = parent
-        self.time= datetime.now()
+        self.time= datetime.today()
         self.DiemDanh()
         self.refresh_list(self.time)
 
